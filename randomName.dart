8862a8119fff6c140359642f4ed5d0781c9f048e
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-10 16:28:09 -0500

    Testing with more methods

--- randomName.dart
+++ randomName.dart
@@ -2,5 +2,27 @@ import 'dart:io';
 
 String nombre(var cuerpoNombre, var edad)
 {
-    return cuerpoNombre+" y tiene "+edad.toString();
+    return print(cuerpoNombre+" y tiene "+edad.toString());
+}
+
+void main()
+{
+    print("Escriba su nombre: ");
+    var name = stdin.readLineSync()!;
+
+    print("Escriba su edad: ");
+    var edad = stdin.readLineSync()!;
+
+    var age = int.parse(edad);
+
+    if(age<18)
+    {
+        var info = nombre(name,edad);
+        print("Es menor de edad");
+    }
+    else if(age>18)
+    {
+        var info = nombre(name,edad);
+        print("Es mayor de edad");
+    }
 }
