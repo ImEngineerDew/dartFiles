832681f1bbe1767b203736e3b09f68c0515f5b4e
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-11 11:07:27 -0500

    Adding a sum operator

--- switchCase.dart
+++ switchCase.dart
@@ -1,9 +1,23 @@
 import 'dart:io';
+import 'dart:mirrors';
 
 int casesOpt(int option) {
   switch (option) {
     case 1:
-      stdout.writeln("Vainilla");
+      stdout.write("Write the value of a: ");
+      var valueA = stdin.readLineSync()!;
+
+      stdout.write("Write the value of b: ");
+      var valueB = stdin.readLineSync()!;
+
+      var a = int.parse(valueA);
+      var b = int.parse(valueB);
+
+      for (; a < b; a++) {
+        var result = a + b;
+        stdout.writeln(
+            a.toString() + "+" + b.toString() + "=" + result.toString());
+      }
       break;
     case 2:
       stdout.writeln("Chocolate");
