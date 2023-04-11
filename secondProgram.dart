05b36689584b813196945fcacf27c08054fa68f5
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-11 09:43:24 -0500

    I have added a stdout.writeln() and stout.write() like a System.out.println() and print() in Java

--- secondProgram.dart
+++ secondProgram.dart
@@ -1,32 +1,29 @@
 import 'dart:io';
 
-void main()
-{
-    print("Please write the value of A: ");
-    var stringA = stdin.readLineSync()!;
+void main() {
+  stdout.write("Please write the value of A: ");
+  String stringA = stdin.readLineSync()!;
 
-    print("Please write the value of B: ");
-    var stringB = stdin.readLineSync()!;
+  stdout.write("Please write the value of B: ");
+  String stringB = stdin.readLineSync()!;
 
-    var a = int.parse(stringA);
-    var b = int.parse(stringB);
+  var a = int.parse(stringA);
+  var b = int.parse(stringB);
 
-    var varSum = a+b;
-    var varRes = a-b;
-    var varMul = a*b;
-    var varMod = a%b;
+  var varSum = a + b;
+  var varRes = a - b;
+  var varMul = a * b;
+  var varMod = a % b;
 
-    if(a>b)
-    {
-        print("The value "+a.toString()+" is greater than "+b.toString());
-    }
-    else
-    {
-        print("The value "+a.toString()+" is lesser than "+b.toString());
-    }
+  if (a > b) {
+    print("The value " + a.toString() + " is greater than " + b.toString());
+  } else {
+    print("The value " + a.toString() + " is lesser than " + b.toString());
+  }
 
-    print("Sum: "+varSum.toString());
-    print("Rest: "+varRes.toString());
-    print("Mult: "+varMul.toString());
-    print("Mod: "+varMod.toString());
+  stdout.writeln(" ");
+  print("Sum: " + varSum.toString());
+  print("Rest: " + varRes.toString());
+  print("Mult: " + varMul.toString());
+  print("Mod: " + varMod.toString());
 }
