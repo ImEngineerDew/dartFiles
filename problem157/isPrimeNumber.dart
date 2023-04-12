ef323672155f1001ad7653dd89b974d6eec5f2a7
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 09:26:12 -0500

    Primer number under construction

--- problem157/isPrimeNumber.dart
+++ problem157/isPrimeNumber.dart
@@ -1,7 +1,21 @@
 import 'dart:io';
 
 int isPrime(int number) {
-  return 0;
+  if (number == 0 || number == 1) {
+    return 0;
+  }
+  if (number == 5) {
+    return 0;
+  }
+  return 1;
 }
 
-void main() {}
+void main() {
+  stdout.write("Check if the next number is prime or not: ");
+  var number = stdin.readLineSync()!;
+  int n = int.parse(number);
+
+  var res = isPrime(n);
+
+  stdout.writeln(res);
+}
