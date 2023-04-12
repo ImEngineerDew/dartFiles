836c3310204b1a7d0a8c210929dee0edac79418f
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 10:09:42 -0500

    Problem partially solved

--- problem157/isPrimeNumber.dart
+++ problem157/isPrimeNumber.dart
@@ -1,17 +1,30 @@
 import 'dart:io';
-import 'dart:math';
 
-void main() {}
+void main() {
+  stdout.write("Check if the number is prime or not: ");
+  var n = stdin.readLineSync()!;
 
-bool isPrime(int alpha, int beta) {
-  if (alpha <= 1 || alpha == 4) {
+  int number = int.parse(n);
+
+  bool check = isPrime(number);
+
+  if (check == true) {
+    print(n + " it's a prime number");
+  } else {
+    print(n + " not a prime number");
+  }
+}
+
+bool isPrime(int number) {
+  //check if the condition returns a false value
+  if (number == 0 || number == 1 || number == 4) {
     return false;
   }
-  if (alpha <= 3) {
-    return true;
+  for (int i = 2; i < (number) / 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
   }
-  int delta = alpha - 1;
-  while (delta % 2 == 0) {
-    delta ~/= 2;
-  }
+  //If the answer is true that indicates a prime number
+  return true;
 }
