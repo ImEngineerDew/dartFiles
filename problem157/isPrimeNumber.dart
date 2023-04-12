fa6ef139570a92b777e8d791097b08df761178f8
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 09:46:15 -0500

    Improve the primes calculations

--- problem157/isPrimeNumber.dart
+++ problem157/isPrimeNumber.dart
@@ -2,3 +2,16 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {}
+
+bool isPrime(int alpha, int beta) {
+  if (alpha <= 1 || alpha == 4) {
+    return false;
+  }
+  if (alpha <= 3) {
+    return true;
+  }
+  int delta = alpha - 1;
+  while (delta % 2 == 0) {
+    delta ~/= 2;
+  }
+}
