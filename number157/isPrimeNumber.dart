8f481e61c5ee9686093565bcd8d99b8c7308cacd
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 17:35:20 -0500

    Almost is ready

--- number157/isPrimeNumber.dart
+++ number157/isPrimeNumber.dart
@@ -4,7 +4,16 @@ import 'dart:io';
 void main() {
   stdout.write("Please write the amount of your array: ");
   int size = int.parse(stdin.readLineSync()!);
-  List<BigInt> numbers = 
+
+  //Defined the dynamic lists that define their size
+  var inputNumbers = List<BigInt>.filled(size, BigInt.zero);
+  var isPrime = List<bool>.filled(size, false);
+
+  for (int i = 0; i < size; i++) {
+    stdout.write("Please write the follow numbers: ");
+    BigInt primes = BigInt.parse(stdin.readLineSync()!);
+    inputNumbers[i] = isPrime(primes);
+  }
 }
 
 bool isPrime(BigInt num) {
