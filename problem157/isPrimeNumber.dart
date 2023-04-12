69cb87fb593c1453eccc80b29b3421c43136e30d
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 09:37:59 -0500

    Restarting the prime numbers

--- problem157/isPrimeNumber.dart
+++ problem157/isPrimeNumber.dart
@@ -1,21 +1,4 @@
 import 'dart:io';
+import 'dart:math';
 
-int isPrime(int number) {
-  if (number == 0 || number == 1) {
-    return 0;
-  }
-  if (number == 5) {
-    return 0;
-  }
-  return 1;
-}
-
-void main() {
-  stdout.write("Check if the next number is prime or not: ");
-  var number = stdin.readLineSync()!;
-  int n = int.parse(number);
-
-  var res = isPrime(n);
-
-  stdout.writeln(res);
-}
+void main() {}
