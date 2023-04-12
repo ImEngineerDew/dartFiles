db6862af9e76cac518e85d7cfd01df0fa5802e05
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-12 17:05:04 -0500

    Putting comments on Dart program

--- number157/isPrimeNumber.dart
+++ number157/isPrimeNumber.dart
@@ -5,10 +5,15 @@ bool isPrime(BigInt num) {
   if (num == BigInt.zero || num == BigInt.one || num.toInt() == 4) {
     return false;
   }
+  //This for cycle tracks the amount of numbers to check
   for (int i = 2; i < numFor; i++) {
     if (numFor % i == 0) {
       return false; //Check if the number isn't prime
     }
   }
   return true; //Check if the number is prime
+}
+
+int emripNumber(BigInt emrip) {
+  return 0;
 }
