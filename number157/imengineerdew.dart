1c30b40ddd524015c3d92a8a2dfc08c6aed9f79e
Dewin Acosta <dewin.acosta9207@gmail.com>
2023-04-15 18:05:04 -0500

    New and  modified files

--- number157/imengineerdew.dart
+++ number157/imengineerdew.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:js_util';
 import 'dart:math';
 
 void main() {
   stdout.write("Please write the size of your array: ");
   int size = int.parse(stdin.readLineSync()!);
   List<BigInt> arrayBigs = List<BigInt>.filled(size, BigInt.zero);
+  readBigsVec(arrayBigs, 0);
+  showBigsVec(arrayBigs, 0);
 }
 
 void readBigsVec(List<BigInt> vec, int index) {
@@ -20,5 +23,21 @@ void showBigsVec(List<BigInt> vec, int index) {
     BigInt result = vec[index];
     stdout.writeln(result);
     showBigsVec(vec, index + 1); //My second recursive Method in Dart
+  }
+}
+
+void nextProbablePrime() {
+  BigInt.zero;
+}
+
+BigInt Omirp(BigInt number) {
+  BigInt answer = isPrime(number);
+  return answer;
+}
+
+void whileOne(BigInt d, int s) {
+  if (d.remainder(BigInt.two) == BigInt.zero) {
+    d = divide(d, BigInt.two);
+    whileOne(d, s + 1);
   }
 }
